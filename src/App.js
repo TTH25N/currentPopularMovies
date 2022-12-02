db3fad330418b87c3b5b0577d98645b974789502
ThomasHuynh <Thomas_TTH@live.com>
2022-12-02 17:34:09 -0500

    implemented api call to return current most popular movie posters

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+import {useEffect, useState} from 'react';
+import axios from 'axios';
+import MovieFrame from './MovieFrame';
 import './App.css';
 
 function App() {
+
+  const [movie, setMovie] = useState([]);
+  const movieUrl = "https://www.themoviedb.org/t/p/w220_and_h330_face"
+  
+  useEffect( () => {
+
+    axios({
+      url: "https://api.themoviedb.org/3/movie/popular",
+        params: {
+          api_key: "02a015f767f49fbd46124014022d6a5c"
+        }
+    })
+      .then((res) => {
+
+        const moviePoster = res.data.results.map((getMovie) => {
+          return movieUrl + getMovie.poster_path
+        })
+
+        setMovie(moviePoster);
+        console.log(movie);
+      })
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Current Popular Movies</h1>
+        <MovieFrame movie={movie}/>
       </header>
     </div>
-  );
+  )
 }
 
 export default App;
+
+
+// Create a variable to store the popular movies link
+// Create a piece of state to hold the final url for each movie poster to be rendered on the page with the movie data (overview, release date, average rating)
+// Assign two variables, one to hold the movie title and the other to hold the movie data which will be displayed when the button is clicked
+// Make a call to the API to return the ten current most popular movies
+// Once the data is returned, map through each movie and retrieve the movie poster endpoint
+// Return the movie poster endpoint appended to the end of the popular movies URL
+// Create a new component for the movie frame which will hold each movie poster, title, and button
+// Create an onClick event for the button to trigger a display of the movie overview, release date, and average rating
+
---
+++ src/MovieFrame.js
@@ -0,0 +1,10 @@
+
+const MovieFrame = (props) => {
+    return (
+        <div>
+            <img src={props.movie} alt="" />
+        </div>
+    )
+}
+
+export default MovieFrame;
