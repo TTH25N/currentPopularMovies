29d8561a7caa4b725c3ef541a2c9d562450e32a8
ThomasHuynh <Thomas_TTH@live.com>
2022-12-06 01:50:37 -0500

    updated styling, added footer

--- src/App.js
+++ src/App.js
@@ -9,11 +9,11 @@ function App() {
   const [movieData, setMovieData] = useState([]);
   const [userChoice, setUserChoice] = useState({});
   const [trailer, setTrailer] = useState("");
-  const [landingPage, setLandingPage] = useState("");
+  const [placeholder, setPlaceholder] = useState([])
 
   const getMovieData = () => {
     axios({
-      url: "https://api.themoviedb.org/3/movie/popular",
+      url: "https://api.themoviedb.org/3/movie/now_playing",
         params: {
           api_key: "02a015f767f49fbd46124014022d6a5c"
         }
@@ -23,46 +23,76 @@ function App() {
         setMovieData(res.data.results)
 
         // setUserChoice will be used to store the movie that the user has selected corresponding to the "See details" that is clicked on
-        setUserChoice(res.data.results)
+        setUserChoice(res.data.results[0])
+        setPlaceholder(res.data.results)
+
       })
   } 
 
+  const getLandingPageVideo = () => {
+    axios({
+      url: `https://api.themoviedb.org/3/movie/${movieData[0].id}/videos`,
+        params: {
+          api_key: "02a015f767f49fbd46124014022d6a5c",
+          append_to_response: "videos"
+        }
+    })
+      .then((res) => {
+          console.log(res.data.results);
+
+          const landingPageTrailer = res.data.results.filter((landingTrailer) => {
+            return landingTrailer.name === "Official Trailer" || landingTrailer.type === "Trailer"
+          })
+
+          setTrailer(landingPageTrailer[0].key)
+      })
+  }
+
 
   useEffect( () => {
-    
     getMovieData();
 
   }, [])
 
 
+  // useEffect( () => {
+  //   if(movieData.length !== 0) {
+  //     getLandingPageVideo()
+  //   }
+
+  // }, [userChoice])
+
+
   return (
-    <div className='app wrapper'>
+    <div className='app'>
 
-      <header className='movieInfoAndVideo'>
-        <div className='landingPage'>
-            <p>Welcome to Miniplex! A place where you can find movie trailers for the latest popular movies. Select see details for any of the movies below!</p>
-        </div>
+      <header className='wrapper'>
+        <h1>Welcome to Miniplex</h1>
+        <div className='movieInfoAndVideo'>
 
-        <div className='movieInfo'>
-          <h1 className='title'>{userChoice.title}</h1>
-          <p className='overview'>{userChoice.overview}</p>
-          <p className='releaseDate'>{userChoice.release_date}</p>
-          <p className='rating'>{userChoice.vote_average}</p>
-        </div>
-
-        <div className='videoContainer'>
-          <YouTube 
-            className={'youtubePlayer'}
-            videoId={trailer}
-            const opts={{
-              width: '100%',
-              height: '100%'
-            }}
-          />
+          <div className='movieInfo'>
+            <h2 className='movieTitle'>{userChoice.title}</h2>
+            <p className='overview'>{userChoice.overview}</p>
+            <p className='releaseDate'>Date of Release: {userChoice.release_date}</p>
+            <p className='rating'>{userChoice.vote_average}</p>
           </div>
+
+          <div className='videoContainer'>
+            <YouTube 
+              className={'youtubePlayer'}
+              videoId={trailer}
+              opts={{
+                width: '100%',
+                height: '100%'
+              }}
+            />
+          </div>
+        </div>
       </header>
 
-      <ul className='movieContainer'>
+      <h2 className='movieListTitle'>Here's What's Playing</h2>
+
+      <ul className='movieListContainer wrapper'>
         {movieData.map((movie) => {
           return (
             <MovieFrame
@@ -75,6 +105,10 @@ function App() {
         })}
       </ul>
 
+      <footer>
+        <p>Created at <a href='https://junocollege.com/'>Juno College</a> using the MovieDB API</p>
+        <p>By Thomas Huynh</p>
+      </footer>
     </div>
   )
 }
